a4d1facf3dc51ffef2151d0481c866c8578cb308
thehiddenn <momisthebest2019@gmail.com>
2024-08-26 09:07:22 -0700

    Database

--- backupScript.ps1
+++ backupScript.ps1
@@ -10,7 +10,6 @@ $backup_file = "$backup_path\db_backup.sql"
 
 # Git repository details
 $git_repo = "C:\Users\Administrator\Desktop\server\Backup"
-$git_branch = "main" # or the branch you want to push to
 
 # Step 1: Export the database
 $mysqldump = "mysqldump --user=$user --password=$password --host=$db_host $db_name > $backup_file"
@@ -24,4 +23,4 @@ git add .
 git commit -m "Database"
 
 # Step 5: Push the backup to GitHub
-git push origin $git_branch
+git push --all
