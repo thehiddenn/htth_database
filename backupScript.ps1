968c720bee3f70ce1f57f3ab8ff26a17d6ae30eb
thehiddenn <momisthebest2019@gmail.com>
2024-08-31 07:01:58 -0700

    Database

--- backupScript.ps1
+++ backupScript.ps1
@@ -6,15 +6,15 @@ $db_name = "htth"
 
 # Backup destination
 $backup_path = "C:\Users\Administrator\Desktop\server\Backup"
-$backup_file = "$backup_path\db_backup.sql"
+$backup_file = "$backup_path\db_backup"
 $backup_zip = "$backup_path\db_htth.sql.gz"
 
 # Git repository details
 $git_repo = "C:\Users\Administrator\Desktop\server\Backup"
 
 # Step 1: Export the database 
-#$mysqldump = "mysqldump --default-character-set=utf8mb4 --user=$user --password=$password --host=$db_host $db_name > $backup_file"
-$mysqldump = "mysqldump --default-character-set=utf8mb4 --skip-comments --user=$user --password=$password --host=$db_host $db_name | 7z > $backup_zip"
+$mysqldump = "mysqldump --default-character-set=utf8mb4 --user=$user --password=$password --host=$db_host $db_name > $backup_file"
+# $mysqldump = "mysqldump --default-character-set=utf8mb4 --skip-comments --user=$user --password=$password --host=$db_host $db_name | 7z > $backup_zip"
 # $mysqldump = "mysqldump --default-character-set=utf8mb4 --user=$user --password=$password --host=$db_host $db_name | tar > $backup_zip"
 Invoke-Expression $mysqldump
 
