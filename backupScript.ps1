d990d3fe7b7f0c24426d1d190b5d0ac0c737a95d
thehiddenn <momisthebest2019@gmail.com>
2024-08-31 05:44:01 -0700

    Database

--- backupScript.ps1
+++ backupScript.ps1
@@ -14,7 +14,7 @@ $git_repo = "C:\Users\Administrator\Desktop\server\Backup"
 
 # Step 1: Export the database 
 #$mysqldump = "mysqldump --default-character-set=utf8mb4 --user=$user --password=$password --host=$db_host $db_name > $backup_file"
-$mysqldump = "mysqldump --default-character-set=utf8mb4 --user=$user --password=$password --host=$db_host $db_name | 7z > $backup_zip"
+$mysqldump = "mysqldump --default-character-set=utf8mb4 --skip-comments --user=$user --password=$password --host=$db_host $db_name | 7z > $backup_zip"
 # $mysqldump = "mysqldump --default-character-set=utf8mb4 --user=$user --password=$password --host=$db_host $db_name | tar > $backup_zip"
 Invoke-Expression $mysqldump
 
